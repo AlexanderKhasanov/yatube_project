06f4aac9aa9ba53a6d4f7fa82d81beaab085afed
Хасанов Александр <AlexanderKhasanov1205@yandex.ru>
2023-03-07 00:45:36 +0300

    add db, styles, pages, templates

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('groups/', views.show_groups_list),
-    path('groups/<slug:slug>/', views.group_posts),
+    path('', views.index, name='main'),
+    path('groups/', views.group_posts, name='group_list'),
+    #path('groups/', views.show_groups_list),
+    #path('groups/<slug:slug>/', views.group_posts),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Главная страница')
-
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+'''
 def show_groups_list(request):
-    return HttpResponse('Список групп')
+    template = 'posts/group_list.html'
+    return render(request, template)
 
 def group_posts(request, slug):
-    return HttpResponse('Посты группы')
+    template = 'posts/group_list.html'
+    return render(request, template)
+    '''
+
+def group_posts(request):
+    template = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)

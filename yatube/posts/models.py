6f98adfad7c4ca29e9995fdba87f13919c329598
Хасанов Александр <AlexanderKhasanov1205@yandex.ru>
2023-03-20 00:48:58 +0300

    t-11 l-3

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,28 +3,58 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=20, unique=True)
-    description = models.TextField()
+    title = models.CharField(
+        max_length=200,
+        verbose_name='group name',
+        help_text='The group name cannot exceed 200 characters'
+    )
+    slug = models.SlugField(
+        max_length=20,
+        unique=True,
+        verbose_name='the unique address of the group',
+        help_text='Slug cannot exceed 200 characters. Slug must be unique'
+    )
+    description = models.TextField(verbose_name='text describing the group')
+
+    class Meta:
+        verbose_name = 'group'
+        verbose_name_plural = 'groups'
 
     def __str__(self) -> str:
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(verbose_name='The text of the post')
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Date of publication of the post',
+        help_text='The publication date is added automatically'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='The author of the post'
     )
-    
     group = models.ForeignKey(
         Group,
-        on_delete=models.CASCADE,
-        related_name='groups',
+        on_delete=models.SET_NULL,
+        related_name='group',
         blank=True,
-        null=True
+        null=True,
+        verbose_name='The group to which the post belongs',
+        help_text=('Select the group to which the post belongs.'
+                   'The field may be empty')
     )
+
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return (f'Author Post - {self.author}. '
+                f'Date of publication - {self.pub_date}')
--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,6 +6,4 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
-    #path('groups/', views.show_groups_list),
-    #path('groups/<slug:slug>/', views.group_posts),
 ]

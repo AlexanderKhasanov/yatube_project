981a1b9ece477b4abde4350c03afefead1f7ef24
Хасанов Александр <AlexanderKhasanov1205@yandex.ru>
2023-04-28 09:38:14 +0300

    add test test_urls_uses_correct_template

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -32,7 +32,7 @@ class PostURLTests(TestCase):
         """Общедоступные страницы доступны любому пользователю"""
         public_pages = [
             '/',
-            '/group/test-slug/',
+            f'/group/{PostURLTests.group.slug}/',
             f'/profile/{PostURLTests.user.get_username()}/',
             f'/posts/{PostURLTests.post.pk}/',
         ]
@@ -55,9 +55,9 @@ class PostURLTests(TestCase):
 
     def test_post_edit_url_exists_at_desired_location_author(self):
         """Страница /post/<post_id>/edit/ доступна автору"""
-        print(f'______/post/{PostURLTests.user.posts.all()[0]}/edit/_____')
+        post_id = PostURLTests.user.posts.all()[0].pk
         response = self.authorized_client.get(
-            f'/post/{PostURLTests.user.posts.all()[0].pk}/edit/'
+            f'/posts/{post_id}/'
         )
         self.assertEqual(response.status_code, HTTPStatus.OK.value)
 
@@ -68,7 +68,41 @@ class PostURLTests(TestCase):
         user_2 = User.objects.create_user(username='User_2')
         no_author = Client()
         no_author.force_login(user_2)
+        post_id = PostURLTests.user.posts.all()[0].pk
         response = no_author.get(
-            f'/post/{PostURLTests.user.posts.all()[0]}/edit/'
+            f'/posts/{post_id}/edit/'
         )
-        self.assertEqual(response.status_code, HTTPStatus.OK.value)
+        self.assertRedirects(response, (f'/posts/{post_id}/'))
+
+    def test_post_edit_url_redirect_anonymous(self):
+        """Страница /post/<post_id>/edit/ перенаправляет анонимного
+        пользователя на страницу авторизации
+        """
+        post_id = PostURLTests.user.posts.all()[0].pk
+        response = self.guest_user.get(
+            f'/posts/{post_id}/edit/'
+        )
+        self.assertRedirects(
+            response, (f'/auth/login/?next=/posts/{post_id}/edit/')
+        )
+
+    def test_non_existent_page(self):
+        """Попытка зайти на несуществующую страницу"""
+        response = self.authorized_client.get('/unexisting_page/')
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND.value)
+
+    def test_urls_uses_correct_template(self):
+        """URL-адрес использует соответствующий шаблон."""
+        url_templates_name = {
+            '/': 'posts/index.html',
+            '/create/': 'posts/create_post.html',
+            f'/group/{PostURLTests.group.slug}/': 'posts/group_list.html',
+            f'/profile/{PostURLTests.user.get_username()}/':
+                'posts/profile.html',
+            f'/posts/{PostURLTests.post.pk}/': 'posts/post_detail.html',
+            f'/posts/{PostURLTests.post.pk}/edit/': 'posts/create_post.html',
+        }
+        for address, template in url_templates_name.items():
+            with self.subTest(address=address):
+                response = self.authorized_client.get(address)
+                self.assertTemplateUsed(response, template)
